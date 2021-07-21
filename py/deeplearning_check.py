9688f51ea0940a415a0d0a7441ff7f66af255296
Monicakim89 <monicakim89@naver.com>
2021-07-21 17:24:54 +0900

    py for opencv/tensor

---
+++ py/deeplearning_check.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+import math
+import os
+import shutil
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)'
+from IPython.display import display
+import PIL
+from tensorflow.python.client import device_lib
+from sklearn.utils import class_weight
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+from matplotlib import font_manager, rc
+rc('font',family="AppleGothic")
+plt.rcParams["font.family"]="AppleGothic" #plt 한글꺠짐
+plt.rcParams["font.family"]="Arial" #외국어꺠짐
+plt.rcParams['axes.unicode_minus'] = False # 마이너스 부호 출력 설정
+plt.rc('figure', figsize=(10,8))
+
+sns.set(font="AppleGothic", 
+        rc={"axes.unicode_minus":False},
+        style='darkgrid') #sns 한글깨짐
+#그래프 세팅
+font = {'family': 'serif',
+        'color':  'white',
+        'weight': 'normal',
+        'size': 16,
+        }
+
+#마이너스 폰트
+plt.rc('axes', unicode_minus=False) # 마이너스 폰트 설정
+
+#시각화?
+import platform
+platform.system()
+
+# 운영체제별 한글 폰트 설정
+if platform.system() == 'Darwin': # Mac 환경 폰트 설정
+    plt.rc('font', family='AppleGothic')
+elif platform.system() == 'Windows': # Windows 환경 폰트 설정
+    plt.rc('font', family='Malgun Gothic')
+
+#tensorflow
+import tensorflow as tf
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import optimizers
+from tensorflow.keras.preprocessing import image
+
+#keras
+import keras
+from keras import models
+from keras import layers`
+from keras import optimizers
+from keras import backend as K
+
+
+# In[ ]:
+
+
+def gpu_check():
+    print(device_lib.list_local_devices())
+
+
+# In[ ]:
+
+
+def recall(y_target, y_pred):
+    # clip(t, clip_value_min, clip_value_max) : clip_value_min~clip_value_max 이외 가장자리를 깎아 낸다
+    # round : 반올림한다
+    y_target_yn = K.round(K.clip(y_target, 0, 1)) # 실제값을 0(Negative) 또는 1(Positive)로 설정한다
+    y_pred_yn = K.round(K.clip(y_pred, 0, 1)) # 예측값을 0(Negative) 또는 1(Positive)로 설정한다
+
+    # True Positive는 실제 값과 예측 값이 모두 1(Positive)인 경우이다
+    count_true_positive = K.sum(y_target_yn * y_pred_yn) 
+
+    # (True Positive + False Negative) = 실제 값이 1(Positive) 전체
+    count_true_positive_false_negative = K.sum(y_target_yn)
+
+    # Recall =  (True Positive) / (True Positive + False Negative)
+    # K.epsilon()는 'divide by zero error' 예방차원에서 작은 수를 더한다
+    recall = count_true_positive / (count_true_positive_false_negative + K.epsilon())
+
+    # return a single tensor value
+    return recall
+
+
+def precision(y_target, y_pred):
+    # clip(t, clip_value_min, clip_value_max) : clip_value_min~clip_value_max 이외 가장자리를 깎아 낸다
+    # round : 반올림한다
+    y_pred_yn = K.round(K.clip(y_pred, 0, 1)) # 예측값을 0(Negative) 또는 1(Positive)로 설정한다
+    y_target_yn = K.round(K.clip(y_target, 0, 1)) # 실제값을 0(Negative) 또는 1(Positive)로 설정한다
+
+    # True Positive는 실제 값과 예측 값이 모두 1(Positive)인 경우이다
+    count_true_positive = K.sum(y_target_yn * y_pred_yn) 
+
+    # (True Positive + False Positive) = 예측 값이 1(Positive) 전체
+    count_true_positive_false_positive = K.sum(y_pred_yn)
+
+    # Precision = (True Positive) / (True Positive + False Positive)
+    # K.epsilon()는 'divide by zero error' 예방차원에서 작은 수를 더한다
+    precision = count_true_positive / (count_true_positive_false_positive + K.epsilon())
+
+    # return a single tensor value
+    return precision
+
+#따로 작성해줘야함
+def f1score(_recall, _precision):
+    _f1score = ( 2 * _recall * _precision) / (_recall + _precision+ K.epsilon())
+    # return a single tensor value
+    return _f1score
+
+
+#feature 넣어주기
+def metrix_check(features):
+    _loss, _acc, _precision, _recall, _fp, _fn  = model.evaluate(features, batch_size=batch_size)
+    return print('\n loss: {:.3f}, accuracy: {:.3f}, precision: {:.3f}, recall: {:.3f}, false_positive: {:.3f}, false_negative: {:.3f}, f1score: {:.3f}'      .format(_loss, _acc, _precision, _recall, _fp, _fn, f1score(_recall, _precision)))
+
+
+# In[ ]:
+
+
+
+
